0ce7433d1a702b93780b62a7c2b31931a4c13c9d
Elias Koch <kochkin.ilya@gmail.com>
2020-09-03 00:22:24 +0300

    финальный вариант

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-
 $settings = require "settings.php";
-
-
 //решение №1
 function config($optionValue, $defaultValue = NULL)
 {
@@ -20,42 +17,20 @@ function config($optionValue, $defaultValue = NULL)
             break;
         }
     }
-   return returnData($optionValue,$defaultValue,$returnVal);
+    return returnData($optionValue, $defaultValue, $returnVal);
 
 }
-function returnData($optionValue,$defaultValue,$returnVal){
+
+//решение №2
+function config1($optionValue, $defaultValue = NULL)
+{
     global $settings;
-    if ($defaultValue && !$returnVal) { // если есть значение по умолчанию то мы его и не существует элемент настройки то мы его создаем
-        $defVal = $defaultValue;
-        $vals1 = explode('.', $optionValue);//вновь получаем вложенный массив
-        $reversVal = array_reverse($vals1);// разворачиваем вложенность в обратном порядке
-        $data = array();
-        foreach ($reversVal as $item) {
-            $data = array($item => $defVal);//добавляем в обратном порядке значения массива
-            $defVal = $data;
-        }
+    $vals = explode('.', $optionValue); //$optionValue - разбиваем на массивы по делителю
+    $count = count($vals);
+    $returnVal = getData($settings[$vals[0]], $vals, $count, 0);//     используя рекурсию мы добираемся донужного нам элемента если у нас нет такого элеента в функцию вернется нулл если есть то значение
+    return returnData($optionValue, $defaultValue, $returnVal);
 
-        $settingsNew = array_merge_recursive($settings, $data);//объеденяем рекурсивно массивы настроек и нового значнеия
-        return [
-            'defaultValue' => $defaultValue,
-            'newSettings' => $settingsNew
-        ];
-    }elseif ($returnVal&&!$defaultValue){
-        return [
-            'returnVal' => $returnVal,
-        ];
-    }elseif (!$returnVal&&!$defaultValue){
-        throw new Exception('Ошибка - не существует параметра '.$optionValue .' настройки и не задано значение по умолчанию');
-    }
 }
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
 
 function getData($current, $options, $count, $pos)
 {
@@ -76,41 +51,58 @@ function getData($current, $options, $count, $pos)
     }
 }
 
-function config1($optionValue, $defaultValue = NULL)
+function returnData($optionValue, $defaultValue, $returnVal)
 {
     global $settings;
-    //решение №2
-    $vals = explode('.', $optionValue); //$optionValue - разбиваем на массивы по делителю
-    $count = count($vals);
-    $returnVal = getData($settings[$vals[0]], $vals, $count, 0);//     используя рекурсию мы добираемся донужного нам элемента если у нас нет такого элеента в функцию вернется нулл если есть то значение
-    return returnData($optionValue,$defaultValue,$returnVal);
+    if ($defaultValue && !$returnVal) { // если есть значение по умолчанию то мы его и не существует элемент настройки то мы его создаем
+        $defVal = $defaultValue;
+        $vals1 = explode('.', $optionValue);//вновь получаем вложенный массив
+        $reversVal = array_reverse($vals1);// разворачиваем вложенность в обратном порядке
+        $data = array();
+        foreach ($reversVal as $item) {
+            $data = array($item => $defVal);//добавляем в обратном порядке значения массива
+            $defVal = $data;
+        }
 
+        $settingsNew = array_merge_recursive($settings, $data);//объеденяем рекурсивно массивы настроек и нового значнеия
+        return [
+            'defaultValue' => $defaultValue,
+            'newSettings' => $settingsNew
+        ];
+    } elseif ($returnVal && !$defaultValue) {
+        return [
+            'returnVal' => $returnVal,
+        ];
+    } elseif (!$returnVal && !$defaultValue) {
+        throw new Exception('Ошибка - не существует параметра ' . $optionValue . ' настройки и не задано значение по умолчанию');
+    }
 }
+
 
 try {
     echo '<pre> ';
-    echo '1) Решение через цикл '. "\n";
-    var_dump( config("site_url")); // http://mysite.ru
-    var_dump( config("db.user")); // admin
-    var_dump( config("app.services.resizer.fallback_format")); // jpeg
-    var_dump( config("db.host", "localhost")); // localhost
-    var_dump( config("db.host2")); // localhost
+    echo '1) Решение через цикл ' . "\n";
+    var_dump(config("site_url")); // http://mysite.ru
+    var_dump(config("db.user")); // admin
+    var_dump(config("app.services.resizer.fallback_format")); // jpeg
+    var_dump(config("db.host", "localhost")); // localhost
+    var_dump(config("db.host2")); // localhost
     echo '</pre>';
-} catch (Exception $e){
+} catch (Exception $e) {
     echo "Произошло исключение: " . $e->getMessage();
 }
 try {
     echo '<pre> ';
-    echo  "\n";
-    echo  "\n";
-    echo '2) Решение через рекурсию '. "\n";
-    var_dump( config1("site_url")); // http://mysite.ru
-    var_dump( config1("db.user")); // admin
-    var_dump( config1("app.services.resizer.fallback_format")); // jpeg
-    var_dump( config1("db.host", "localhost")); // localhost
-    var_dump( config1("db.host3")); // localhost
+    echo "\n";
+    echo "\n";
+    echo '2) Решение через рекурсию ' . "\n";
+    var_dump(config1("site_url")); // http://mysite.ru
+    var_dump(config1("db.user")); // admin
+    var_dump(config1("app.services.resizer.fallback_format")); // jpeg
+    var_dump(config1("db.host", "localhost")); // localhost
+    var_dump(config1("db.host3")); // localhost
 
     echo '</pre>';
-} catch (Exception $e){
+} catch (Exception $e) {
     echo "Произошло исключение: " . $e->getMessage();
 }
